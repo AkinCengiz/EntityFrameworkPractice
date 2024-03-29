0ee6b712f7b35db00d57aab9f747a77c0c4562b9
akincengiz <akincengiz@outlook.com.tr>
2024-03-29 16:03:13 +0300

    Join operation add

--- EntityFrameworkPractice.KeylessEntityType/Program.cs
+++ EntityFrameworkPractice.KeylessEntityType/Program.cs
@@ -1,4 +1,6 @@
-﻿using EntityFrameworkPractice.KeylessEntityType.DataAccess;
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using EntityFrameworkPractice.KeylessEntityType.DataAccess;
 using EntityFrameworkPractice.KeylessEntityType.Entity;
 
 namespace EntityFrameworkPractice.KeylessEntityType;
@@ -37,33 +39,63 @@ internal class Program
             //    Console.WriteLine("Success\n");
             //}
 
-            var result = (from c in context.Categories
+            //var result = (from c in context.Categories
+            //    join p in context.Products on c.Id equals p.CategoryId
+            //    join pf in context.ProductFeatures on p.Id equals pf.Id
+            //    select new
+            //    {
+            //        CategoryName = c.Name,
+            //        ProductName = p.Name,
+            //        Stock = p.Stock,
+            //        Price = p.Price,
+            //        Color = pf.Color,
+            //        Width = pf.Width,
+            //        Height = pf.Height,
+            //    }).ToList();
+
+            //var result2 = context.Categories.Join(context.Products, x => x.Id,y => y.CategoryId,(c,p) => new {Category=c.Name,Product = p.Name}).ToList();
+
+            //result.ForEach(c =>
+            //{
+            //    Console.WriteLine(
+            //        $"{c.CategoryName} - {c.ProductName} {c.Stock} {c.Price} {c.Color} {c.Width} {c.Height}");
+            //});
+
+            //result2.ForEach(x =>
+            //{
+            //    Console.WriteLine($"{x.Category} => {x.Product}");
+            //});
+
+            var data = (from c in context.Categories
                 join p in context.Products on c.Id equals p.CategoryId
                 join pf in context.ProductFeatures on p.Id equals pf.Id
-                select new
-                {
-                    CategoryName = c.Name,
-                    ProductName = p.Name,
-                    Stock = p.Stock,
-                    Price = p.Price,
-                    Color = pf.Color,
-                    Width = pf.Width,
-                    Height = pf.Height,
-                }).ToList();
+                select new { c, p, pf }).ToList();
 
-            var result2 = context.Categories.Join(context.Products, x => x.Id,y => y.CategoryId,(c,p) => new {Category=c.Name,Product = p.Name}).ToList();
-
-            result.ForEach(c =>
+            data.ForEach(x =>
             {
                 Console.WriteLine(
-                    $"{c.CategoryName} - {c.ProductName} {c.Stock} {c.Price} {c.Color} {c.Width} {c.Height}");
+                    $"****************************\nKategori\t: {x.c.Name}\n****************************\nÜrün Bilgileri\nÜrün Adı\t: {x.p.Name}\nFiyatı\t: {x.p.Price}\n****************************\nStok Durumu\t :{x.p.Stock}\n****************************\nÜrün Özellikleri\nRenk\t: {x.pf.Color}\nGenişlik\t: {x.pf.Width}\nYükseklik\t: {x.pf.Height}");
             });
 
-            result2.ForEach(x =>
+            var data2 = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => new
             {
-                Console.WriteLine($"{x.Category} => {x.Product}");
+                c, p
+            }).Join(context.ProductFeatures, x => x.p.Id, y => y.Id, (c, pf) => new
+            {
+                CategoryName = c.c.Name,
+                ProductName = c.p.Name,
+                Price = c.p.Price,
+                Stock = c.p.Stock,
+                Color = pf.Color,
+                Width = pf.Width,
+                Height = pf.Height
+            }).ToList();
+
+            data2.ForEach(d =>
+            {
+                Console.WriteLine(
+                    $"****************************\nKategori\t: {d.CategoryName}\n****************************\nÜrün Bilgileri\nÜrün Adı\t: {d.ProductName}\nFiyatı\t: {d.Price}\n****************************\nStok Durumu\t :{d.Stock}\n****************************\nÜrün Özellikleri\nRenk\t: {d.Color}\nGenişlik\t: {d.Width}\nYükseklik\t: {d.Height}");
             });
         }
-
     }
 }
